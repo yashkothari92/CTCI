48cd6ca08e129ce36ad082cec2d5bfcc632d5b57
Yash <yashkothari2020@gmail.com>
2020-01-09 23:03:50 +0530

    Contiguous Sequence updated- start & end index returned

--- moderate/ContiguousSequence.java
+++ moderate/ContiguousSequence.java
@@ -3,50 +3,44 @@ package moderate;
  * Given an array, find contiguous sequence with largest sum, Return the sum.
  * */
 
-/*
-class ContiguousSeqResult {
-	private String seq;
-	private int maxSum;
-	
-	public ContiguousSeqResult(String seq, int maxSum) {
-		this.seq = seq.replaceAll(",$", "");
-		this.maxSum = maxSum;
-	}
-	
-	@Override
-	public String toString() {
-		return "Contiguous sequence:("+seq+") with Largest Sum: "+maxSum;
-	}
-}
-*/
 public class ContiguousSequence {
 
 	public static void main(String[] args) {
 		
-		int A[] = { 2, -8, 3, -2, 4, -10};
+		//int A[] = { 2, -8, 3, -2, 4, -10};
+        int A[] = { -2, -3, 4, -1, -2, 1, 5, -3 }; 
 		//int A[] = { 2, 3, -8, -1, 2, 4, -2, 3};
 		
-		int largestSum = findLargestSum(A);
-		System.out.println(largestSum);
+		String largestSum = findLargestSum(A);
+		String[] info = largestSum.split(",");
+		System.out.println("Largest sum is ["+info[0]+"] starting from index "+info[1]+" and ending at "+info[2]);
+		
+		System.out.print("{");
+		for (int i = Integer.parseInt(info[1]); i <= Integer.parseInt(info[2]); i++) {
+			System.out.print(A[i] + " ");
+		}
+		System.out.print("}");
+
 	}
 
-	private static int findLargestSum(int[] a) {
-	//	StringBuilder sb = new StringBuilder();
+	private static String findLargestSum(int[] a) {
+		int s=0, start = 0, end=0;
+			
 		int sum = 0;
 		int maxSum = 0;
 		
 		for(int i=0; i<a.length; i++) {
 			sum += a[i];
-			// sb.append(a[i]+","); 
 
 			if (maxSum < sum) {
 				maxSum = sum;
+				start = s;
+				end = i;
 			} else if(sum < 0){
 				sum = 0;
-			//	sb.setLength(0);
+				s = i+1;
 			}
 		}
-		
-		return maxSum; //new ContiguousSeqResult(sb.toString(), maxSum);
+		return maxSum+","+start+","+end;
 	}
 }
