c35aef6f4af951f5cac02dd5ed826fac5f174d45
Yash <yashkothari2020@gmail.com>
2020-02-26 00:55:19 +0530

    4.2

---
+++ tree_n_graphs/MinimalTree.java
@@ -0,0 +1,46 @@
+package tree_n_graphs;
+
+class TreeNode {
+	int data;
+	TreeNode left, right, parent;
+
+	public TreeNode(int data) {
+		this.data = data;
+	}
+}
+
+public class MinimalTree {
+
+	public static void main(String[] args) {
+		int arr[] = { 1, 3, 5, 7, 9, 11, 13 };
+		TreeNode tree = buildMinimalTree(arr);
+		traverse(tree);
+	}
+
+	/*In-order traversal*/
+	private static void traverse(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+		traverse(root.left);
+		System.out.print(root.data+" ");
+		traverse(root.right);
+	}
+
+	private static TreeNode buildMinimalTree(int[] arr) {
+		return buildMinimalTree(arr, 0, arr.length - 1);
+	}
+
+	private static TreeNode buildMinimalTree(int[] arr, int start, int end) {
+		if (end < start) {
+			return null;
+		}
+		
+		int mid = (start + end) / 2;
+		TreeNode t = new TreeNode(arr[mid]);
+
+		t.left = buildMinimalTree(arr, start, mid - 1);
+		t.right = buildMinimalTree(arr, mid + 1, end);
+		return t;
+	}
+}
